70e4046753daab24ede5c47ca8414c9dcc52c0a7
Henrique Matias de Souza <henrique_matias@virgo.atlantico.net.br>
2020-10-13 20:20:26 -0300

    Initial commit - Add packages

---
+++ src/Controllers/ApiController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResponseHelper.Utils;
+
+namespace ResponseHelper.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiController : ControllerBase
+    {
+        internal IActionResult SimpleResponse(Response response)
+        {
+            IActionResult result;
+
+            if (response.IsSuccess)
+                result = Ok(response.ResponseObject);
+            else
+                result = BadRequest(response.Exception);
+
+            return result;
+        }
+
+        internal IActionResult HttpResponse(Response response)
+        {
+            IActionResult result;
+
+            switch (response.HttpCode)
+            {
+                case 200:
+                    result = Ok(response.ResponseObject);
+                    break;
+                case 400:
+                    result = BadRequest(response.Exception);
+                    break;
+                case 404:
+                    result = NotFound(response.Exception);
+                    break;
+                default:
+                    result = StatusCode(response.HttpCode, response.GetResponseBaseadOnHttpCode());
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/Utils/Response.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ResponseHelper.Utils
+{
+    public class Response
+    {
+        public int HttpCode { get; set; }
+        public bool IsSuccess { get; set; }
+        public Object ResponseObject { get; set; }
+        public Exception Exception { get; set; }
+
+        public Object GetResponseBaseadOnHttpCode()
+        {
+            return IsSuccessHttpCode() ? ResponseObject : Exception;
+        }
+
+        public bool IsSuccessHttpCode()
+        {
+            return (HttpCode >= 200 && HttpCode < 300);
+        }
+    }
+}
